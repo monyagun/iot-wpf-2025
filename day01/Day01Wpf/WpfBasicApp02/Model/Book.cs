37faccdc651c5d0bbb426c1536b1be24356525f2
pgh157 <kh813love@naver.com>
2025-05-08 17:20:52 +0900

    1일차

---
+++ day01/Day01Wpf/WpfBasicApp02/Model/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfBasicApp02.Model
+{
+    class Book : INotifyPropertyChanged
+    {
+        public int Idx {  get; set; }
+        public string Division { get; set; }
+        public string DNames { get; set; }
+        public string Names { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Price { get; set; }
+
+        // 위의 여덟개의 값이 기존상태에서 변경이 되면 발생하는 이벤트
+        public event PropertyChangedEventHandler? PropertyChanged;  // 사용자가 클릭이나 클릭같은거로 발생하는 이벤트가 아님
+    }
+}
