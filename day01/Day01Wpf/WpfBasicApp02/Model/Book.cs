48a1e67651da2ef0c90198e65e0bef6f7db3552e
pgh157 <kh813love@naver.com>
2025-05-09 17:21:06 +0900

    2일차 커밋

--- day01/Day01Wpf/WpfBasicApp02/Model/Book.cs
+++ day01/Day01Wpf/WpfBasicApp02/Model/Book.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -8,9 +7,9 @@ using System.Threading.Tasks;
 
 namespace WpfBasicApp02.Model
 {
-    class Book : INotifyPropertyChanged
+    public class Book : INotifyPropertyChanged
     {
-        public int Idx {  get; set; }
+        public int Idx { get; set; }
         public string Division { get; set; }
         public string DNames { get; set; }
         public string Names { get; set; }
@@ -19,7 +18,7 @@ namespace WpfBasicApp02.Model
         public DateTime ReleaseDate { get; set; }
         public int Price { get; set; }
 
-        // 위의 여덟개의 값이 기존상태에서 변경이 되면 발생하는 이벤트
-        public event PropertyChangedEventHandler? PropertyChanged;  // 사용자가 클릭이나 클릭같은거로 발생하는 이벤트가 아님
+        // 위의 여덟개의 값이 기존상태에서 변경이되면 발생하는 이벤트
+        public event PropertyChangedEventHandler? PropertyChanged; // 사용자가 클릭같은거로 발생하는 이벤트가 아님
     }
 }
--- day02/Day02Wpf/WpfBasicApp02/ViewModels/MainViewModel.cs
+++ day02/Day02Wpf/WpfBasicApp02/ViewModels/MainViewModel.cs
@@ -1,45 +1,42 @@
-﻿using MySql.Data.MySqlClient;
+﻿using Caliburn.Micro;
+using MahApps.Metro.Controls.Dialogs;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WpfBasicApp02.Model;
+using WpfBasicApp02.Models;
 
-namespace WpfBasicApp02.ViewModel
+namespace WpfBasicApp02.ViewModels
 {
-    // MainViewModel에 속하는 속성의 값이 변경되면 이벤트가 발생.
-    public class MainViewModel : INotifyPropertyChanged
+    public class MainViewModel : Conductor<object>
     {
-        // 속성추가 get; set;
-        // ObervableCollection -> 리스트의 변형(변화를 감지할 수 있도록 처리된 클래스)
+        private IDialogCoordinator dialogCoordinator;   // 메시지박스, 다이얼로그 실행을 위한 변수
         public ObservableCollection<Book> Books { get; set; }
-        // List<KeyValuePair<string, string>> divisions의 변형
-        public ObservableCollection<KeyValuePair<string,string>> Divisions { get; set; }
-
-        // 선택된값에 대한 멤버변수, 멤버변수는 _를 붙이거나, 소문자로 변수명을 시작
+        public ObservableCollection<KeyValuePair<string, string>> Divisions { get; set; }
         private Book _selectedBook;
-        // 선택된값에 대한 속성
+        private DialogCoordinator _dialogCoordinator;
+
         public Book SelectedBook
         {
-            get => _selectedBook;   // 람다식  get { return _selectedBook; } 와 동일
+            get => _selectedBook;
             set
             {
                 _selectedBook = value;
-                // 값이 변경된 것을 알아차리도록 해줘야함!!
-                OnPropertyChanged(nameof(SelectedBook));
+                NotifyOfPropertyChange(() => SelectedBook);
             }
         }
 
-        public MainViewModel() {
+        public MainViewModel()
+        {
+            _dialogCoordinator = new DialogCoordinator();
+
             LoadControlFromDb();
             LoadGridFromDb();
         }
 
-        // DB에서 콤보박스에 넣을 데이터를 불러오기
         private void LoadControlFromDb()
         {
             // 1. 연결문자열(DB연결문자열은 필수)
@@ -47,7 +44,6 @@ namespace WpfBasicApp02.ViewModel
             // 2. 사용쿼리
             string query = "SELECT division, names FROM divtbl";
 
-            // Dictionary나 KeyValuePair 둘다 상관없음
             ObservableCollection<KeyValuePair<string, string>> divisions = new ObservableCollection<KeyValuePair<string, string>>();
 
             // 3. DB연결, 명령, 리더
@@ -57,7 +53,7 @@ namespace WpfBasicApp02.ViewModel
                 {
                     conn.Open();
                     MySqlCommand cmd = new MySqlCommand(query, conn);
-                    MySqlDataReader reader = cmd.ExecuteReader();   // 데이터 가져올때
+                    MySqlDataReader reader = cmd.ExecuteReader(); // 데이터 가져올때                    
 
                     while (reader.Read())
                     {
@@ -69,25 +65,24 @@ namespace WpfBasicApp02.ViewModel
                 }
                 catch (MySqlException ex)
                 {
-                   
+                    // 나중에...
                 }
-            }   // conn.Close() 자동 발생
+            } // conn.Close() 자동발생
 
             Divisions = divisions;
-            OnPropertyChanged(nameof(Divisions));   // Divisions 속성값이 변경됨!
+            NotifyOfPropertyChange(() => Divisions);    // Cliburn.Micro가 제공
         }
 
-        // DB에서 데이터 로드후 Books 속성에 집어넣기
         private void LoadGridFromDb()
         {
             // 1. 연결문자열(DB연결문자열은 필수)
             string connectionString = "Server=localhost;Database=bookrentalshop;Uid=root;Pwd=12345;Charset=utf8;";
-            // 2. 사용쿼리, 기본쿼리로 먼저 작업 후 필요한 실제쿼리로 변경해도 됨
+            // 2. 사용쿼리, 기본쿼리로 먼저 작업 후 필요한 실제쿼리로 변경해도
             string query = @"SELECT b.Idx, b.Author, b.Division, b.Names, b.ReleaseDate, b.ISBN, b.Price,
-	                    d.Names AS dNames
-                            FROM bookstbl AS b, divtbl AS d
-                            WHERE b.Division = d.Division
-                            ORDER BY b.Idx";
+                        d.Names AS dNames
+                   FROM bookstbl AS b, divtbl AS d
+                  WHERE b.Division = d.Division
+                  ORDER by b.Idx";
 
             ObservableCollection<Book> books = new ObservableCollection<Book>();
 
@@ -99,7 +94,7 @@ namespace WpfBasicApp02.ViewModel
                     conn.Open();
                     MySqlCommand cmd = new MySqlCommand(query, conn);
                     MySqlDataReader reader = cmd.ExecuteReader();
-                    
+
                     while (reader.Read())
                     {
                         books.Add(new Book
@@ -111,29 +106,23 @@ namespace WpfBasicApp02.ViewModel
                             Author = reader.GetString("Author"),
                             ISBN = reader.GetString("ISBN"),
                             ReleaseDate = reader.GetDateTime("ReleaseDate"),
-                            Price = reader.GetInt32("Price")
-
+                            Price = reader.GetInt32("Price"),
                         });
                     }
-
                 }
                 catch (MySqlException ex)
                 {
                     // 나중에...
                 }
-            }   // conn.Close() 자동 발생
+            } // conn.Close() 자동발생
 
             Books = books;
-            OnPropertyChanged(nameof(Books));
+            NotifyOfPropertyChange(() => Books);
         }
 
-        // 속성값이 변경되면 이벤트를 발생
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        protected void OnPropertyChanged(string name)
+        public async void DoAction()
         {
-            // 기본적인 이벤트핸들러 파라미터와 동일(Object sender, EventArgs e)
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            await _dialogCoordinator.ShowMessageAsync(this, "데이터로드!", "로드");
         }
     }
 }
